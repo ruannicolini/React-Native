61010f8bfc323969fe2fda91ddfc5dfe05274e72
Ruan Nicolini <ruan_03@hotmail.com>
2018-09-08 15:11:18 -0300

    App Loja add

---
+++ 2_Projetos/loja/src/components/ListaItens.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {
+  Text
+} from 'react-native';
+
+export default class loja extends Component {
+  render() {
+    return (
+        <Text>Teste de importação de componente</Text>
+    );
+  }
+}
