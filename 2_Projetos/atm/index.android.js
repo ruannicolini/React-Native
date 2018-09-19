22fe0528c83c5e20f537a42c249c7a55f7871c97
Ruan Nicolini <ruan_03@hotmail.com>
2018-09-18 22:04:00 -0300

    Componente Navigator

--- 2_Projetos/atm/index.android.js
+++ 2_Projetos/atm/index.android.js
@@ -1,15 +1,32 @@
 
 import React, { Component } from 'react';
-import {
-  AppRegistry
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components'
 
 import CenaPrincipal from './src/components/CenaPrincipal';
+import CenaClientes from './src/components/CenaClientes';
 
 export default class atm extends Component {
   render() {
     return (
-      <CenaPrincipal/>
+      <Navigator 
+        initialRoute={ /* Rota Inicial*/ { id: 'a' } }
+        renderScene={(route, navigator) => { 
+            /* Definir a cena com base na rota */
+
+            if(route.id === 'a'){
+              //exibe a cena Principal
+              return( <CenaPrincipal /> );
+            }
+
+            if(route.id === 'b'){
+              //exibe a cena Principal
+              return( <CenaClientes /> );
+            }
+
+        } }
+      />
+
     );
   }
 }
---
+++ 2_Projetos/atm/src/components/CenaClientes.js
@@ -0,0 +1,68 @@
+
+import React, { Component } from 'react';
+import {
+  View,
+  StatusBar,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+import BarraNavegacao from './BarraNavegacao';
+
+const ImagemDetalheCliente = require('../imgs/detalhe_cliente.png');
+const Imagemcliente1 = require('../imgs/cliente1.png');
+const Imagemcliente2 = require('../imgs/cliente2.png');
+
+export default class CenaClientes extends Component {
+    render() {
+        return (
+            <ScrollView>
+                <StatusBar 
+                hidden={false} // visivel = false |  OBS pode-se passar apenas "hidden" sem definir true ou false, nesse caso é false
+                backgroundColor='#B9C941'
+                />
+
+                <BarraNavegacao /> 
+
+                <View style ={styles.ItemPrincipal} >
+                    <Image source={ImagemDetalheCliente} />
+                    <Text style={ styles.LetraItemPrincipal }>Nossos Clientes</Text>
+                </View>
+
+                <View style={ styles.ItemSecundario } >
+                    <Image source={Imagemcliente1} />
+                    <Text style={styles.textoItemSecundario}>Testo de Exemplo 1</Text>
+                </View>
+
+                <View style={ styles.ItemSecundario }  >
+                    <Image source={Imagemcliente2} />
+                    <Text style={styles.textoItemSecundario} >Texto de exemplo 2</Text>
+                </View>
+
+            </ScrollView>
+        );
+      }
+    }
+
+const styles = StyleSheet.create({
+    ItemPrincipal: {
+        marginTop: 20,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    LetraItemPrincipal: {
+        fontSize: 25,
+        color: '#B9C941',
+        marginLeft:10
+    },
+    ItemSecundario: {
+        marginTop: 20,
+        marginLeft: 20
+    },
+    textoItemSecundario: {
+        marginLeft: 20,
+        fontSize: 18
+    }
+})
