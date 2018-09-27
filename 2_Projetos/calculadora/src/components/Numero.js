e83da4692deedb8188b96da7ff6711fbe35f3b20
Ruan Nicolini <ruan_03@hotmail.com>
2018-09-26 22:05:14 -0300

    Componente Entrada ok

---
+++ 2_Projetos/calculadora/src/components/Numero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+export default props => (
+    <View>
+        <TextInput style={styles.numero} />
+    </View>
+);
+
+const styles = StyleSheet.create({
+    numero: {
+        width: 140,
+        height: 80,
+        fontSize: 20    
+    }
+});
--- 2_Projetos/calculadora/src/components/Painel.js
+++ 2_Projetos/calculadora/src/components/Painel.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Entrada from './Entrada';
+import Operacao from './Operacao';
+import { Comando } from './Comando';
+
 const Painel = props => (
     <View>
         <Text>Painel</Text>
+        <Entrada />
+        <Operacao />
+        <Comando />
     </View>
 );
 
