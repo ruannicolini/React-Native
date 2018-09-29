868b0b6ecacde1aec813ba9f9aa1c9d8c2b485c1
Ruan Nicolini <ruan_03@hotmail.com>
2018-09-29 11:29:37 -0300

    Alterando estado de um componente a partir de outro

--- 2_Projetos/calculadora/src/components/Painel.js
+++ 2_Projetos/calculadora/src/components/Painel.js
@@ -4,6 +4,19 @@ import { View, Text } from 'react-native';
 import Entrada from './Entrada';
 import Operacao from './Operacao';
 import { Comando } from './Comando';
+
+//Precisamos obter o valor dos TextInput do componente numero (dentro do componente ENTRADA)
+//e fazer uma acao com ele, encadeada a partir do click no button dentro do componente COMANDO.
+
+// Para isso, devemos cria var's de estados para os numeros (num1 e num2) dentro do componente que
+// hierarquicamente encapsula os 2 componentes(ENTRADA e COMANDO), ou seja: Componente Painel.
+//Entao passamos num1 e num2 como props para o componente ENTRADA, que por sua vez 
+// passará o valor como a props num para o componente NUMERO.
+
+// Quanto a acao desencadeada a partir do click do botao em COMANDO,
+// criamos uma funcao dentro do componente que hierarquicamente encapsula os 2 componentes(ENTRADA e COMANDO), ou seja: Componente Painel.
+// e passamos essa funcao como uma props para o componente COMANDO (props acao).
+// Dentro do componente COMANDO, atribuimos a props acao ao evento onPress do botao. 
  
 class Painel extends Component {
     constructor(props){
