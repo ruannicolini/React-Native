dc4200b2ccfbf5157573d1b1b66ec11aebb8d940
Ruan Nicolini <ruan_03@hotmail.com>
2018-10-22 21:59:43 -0300

    Component tab-view add Principal

--- 2_Projetos/whatsappClone/src/components/Principal.js
+++ 2_Projetos/whatsappClone/src/components/Principal.js
@@ -1,8 +1,79 @@
-import React, {Component} from 'react';
-import { View, Text} from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 
-export default props => (
-    <View style={ {marginTop:100} } >
-        <Text>oi. teste!</Text>    
-    </View>
+const FirstRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
 );
+const SecondRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
+);
+
+export default class Principal extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'First' },
+      { key: 'second', title: 'Second' },
+    ],
+  };
+
+  render() {
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={SceneMap({
+          first: FirstRoute,
+          second: SecondRoute,
+        })}
+        onIndexChange={index => this.setState({ index })}
+        initialLayout={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
+      />
+    );
+  }
+}
+
+// import React, { Component } from 'react';
+// import { View, StyleSheet } from 'react-native';
+// import { TabViewAnimated, SceneMap } from 'react-native-tab-view';
+
+// const FirstRoute = () => <View style={ [ styles.container, { backgroundColor:'#ff4081'} ] } />
+// const SecondRoute = () => <View style={ [ styles.container, { backgroundColor:'#ff4081'} ] } />
+
+// export default class Principal extends Component {
+//   state = {
+//     index: 0,
+//     routes: [
+//       { key: '1', title: 'First' },
+//       { key: '2', title: 'Second' }
+//     ],
+//   };
+
+//   _handleChangeTab = index => this.setState({ index });
+
+//   _renderHeader = props => <TabBarMenu {...props} />;
+
+//   _renderScene = SceneMap({
+//     '1': FirstRoute,
+//     '2': SecondRoute
+//   });
+
+//   render() {
+//     return (
+//       <TabViewAnimated
+//         style={styles.container}
+//         navigationState={this.state}
+//         renderScene={this._renderScene}
+//         renderHeader={this._renderHeader}
+//         onRequestChangeTab={this._handleChangeTab}
+//       />
+//     );
+//   }
+// }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
