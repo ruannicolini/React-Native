59a7f52f8519503cb90f53bca69b8e16a5747b79
Ruan Nicolini <ruan_03@hotmail.com>
2018-11-15 17:33:36 -0200

    Implementando tab viwer - Contatos e Conversas

--- 2_Projetos/whatsappClone/src/components/Principal.js
+++ 2_Projetos/whatsappClone/src/components/Principal.js
@@ -1,80 +1,7 @@
-// import * as React from 'react';
-// import { View, StyleSheet, Dimensions } from 'react-native';
-// import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
-//  const FirstRoute = () => (
-//   <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
-// );
-// const SecondRoute = () => (
-//   <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
-// );
-//  export default class Principal extends React.Component {
-//   state = {
-//     index: 0,
-//     routes: [
-//       { key: 'first', title: 'First' },
-//       { key: 'second', title: 'Second' },
-//     ],
-//   };
-//    render() {
-//     return (
-//       <TabView
-//         navigationState={this.state}
-//         renderScene={SceneMap({
-//           first: FirstRoute,
-//           second: SecondRoute,
-//         })}
-//         onIndexChange={index => this.setState({ index })}
-//         initialLayout={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
-//       />
-//     );
-//   }
-// }
-//  // import React, { Component } from 'react';
-// // import { View, StyleSheet } from 'react-native';
-// // import { TabViewAnimated, SceneMap } from 'react-native-tab-view';
-//  // const FirstRoute = () => <View style={ [ styles.container, { backgroundColor:'#ff4081'} ] } />
-// // const SecondRoute = () => <View style={ [ styles.container, { backgroundColor:'#ff4081'} ] } />
-//  // export default class Principal extends Component {
-// //   state = {
-// //     index: 0,
-// //     routes: [
-// //       { key: '1', title: 'First' },
-// //       { key: '2', title: 'Second' }
-// //     ],
-// //   };
-//  //   _handleChangeTab = index => this.setState({ index });
-//  //   _renderHeader = props => <TabBarMenu {...props} />;
-//  //   _renderScene = SceneMap({
-// //     '1': FirstRoute,
-// //     '2': SecondRoute
-// //   });
-//  //   render() {
-// //     return (
-// //       <TabViewAnimated
-// //         style={styles.container}
-// //         navigationState={this.state}
-// //         renderScene={this._renderScene}
-// //         renderHeader={this._renderHeader}
-// //         onRequestChangeTab={this._handleChangeTab}
-// //       />
-// //     );
-// //   }
-// // }
-//  const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-// });
-
-
-
-
-
-
-
 import * as React from 'react';
 import { View, StyleSheet, Dimensions, Text } from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
+// import TabBarMenu from './TabBarMenu';
 
 const Conversas = () => (
   <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
@@ -100,18 +27,20 @@ export default class Principal extends React.Component {
           first: Conversas,
           second: Contatos,
         })}
-        renderTabBar={ props => 
+      renderTabBar={ props =>
         <View> 
             <Text>WhatsappClone</Text>
             <TabBar {...props} />
         </View> 
-        }
+      }
         onIndexChange={index => this.setState({ index })}
         initialLayout={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
       />
     );
   }
 }
+
+      
 
 // import React, { Component } from 'react';
 // import { View, StyleSheet } from 'react-native';
